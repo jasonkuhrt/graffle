205b43d4fa7c3f8842000cf87e97670cf72cfdd5
Johannes Schickling <schickling.j@gmail.com>
2017-05-27 14:39:49 +0200

    better error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import * as http from 'http'
-import * as https from 'https'
-import { parse } from 'url'
+import fetch, {Response} from 'node-fetch'
 
 export interface NodeQLOptions {
   headers?: { [key: string]: string }
@@ -17,51 +15,37 @@ export default class NodeQLClient {
     }
   }
 
-  query<T extends any>(queryStr: string, variables?: { [key: string]: any }): Promise<T> {
-
-
-    const isHttps = this.url.startsWith('https')
-    const postData = JSON.stringify({
-      query: queryStr,
+  async query<T extends any>(query: string, variables?: { [key: string]: any }): Promise<T> {
+    const body = JSON.stringify({
+      query,
       variables: variables ? variables : undefined,
     })
 
-    const parsedUrl = parse(this.url)
-    const postOptions = {
-      host: parsedUrl.host,
-      port: parseInt(parsedUrl.port || (isHttps ? '443' : '80'), 10),
-      path: parsedUrl.path,
+    const response = await fetch(this.url, {
       method: 'POST',
-      headers: Object.assign({
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(postData),
-      }, this.options.headers),
-    }
-
-
-    return new Promise((resolve, reject) => {
-      const handler = res => {
-        if (res.statusCode !== 200) {
-          reject(new Error(`Bad status code: ${res.statusCode}`))
-        } else {
-          res.setEncoding('utf8')
-          res.on('data', data => {
-            const result = JSON.parse(data.toString())
-            if (result.errors) {
-              reject(new Error(JSON.stringify(result.errors)))
-            } else {
-              resolve(result.data)
-            }
-          })
-        }
-      }
-
-      const req = isHttps
-        ? https.request(postOptions, handler)
-        : http.request(postOptions, handler)
-
-      req.write(postData)
-      req.end()
+      headers: Object.assign({ 'Content-Type': 'application/json' }, this.options.headers),
+      body,
     })
+
+    const result = await getResult(response)
+
+    if (response.ok && !result.errors && !result.error) {
+      return result.data
+    } else {
+      throw new Error(JSON.stringify({
+        status: response.status,
+        result,
+        query,
+        variables,
+      }, null, 2))
+    }
+  }
+}
+
+async function getResult(response: Response): Promise<any> {
+  if (response.headers.get('Content-Type') === 'application/json') {
+    return await response.json()
+  } else {
+    return await response.text()
   }
 }
