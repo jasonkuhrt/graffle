542ebccdfac1725a09bc707ea376afe192a8df14
Jason Kuhrt <jasonkuhrt@me.com>
2023-02-17 22:10:21 -0500

    feat: make package hybrid ESM/CJS closes #192

--- src/graphql-ws.ts
+++ src/graphql-ws.ts
@@ -1,6 +1,7 @@
 import { ClientError, RequestDocument, Variables } from './types'
 import * as Dom from './types.dom'
 import { resolveRequestDocument } from './resolveRequestDocument'
+// import type WebSocket from 'ws'
 
 const CONNECTION_INIT = 'connection_init'
 const CONNECTION_ACK = 'connection_ack'
