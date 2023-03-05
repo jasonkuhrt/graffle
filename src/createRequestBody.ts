f2b3cd58a7a6159db75640385d57cbe798c14e3a
sonofmagic <1324318532@qq.com>
2023-03-06 00:18:28 +0800

    feat: remove some pkgs can't run in weapp env

--- src/createRequestBody.ts
+++ src/createRequestBody.ts
@@ -1,17 +1,17 @@
 import { defaultJsonSerializer } from './defaultJsonSerializer.js'
-import type * as Dom from './types.dom.js'
+// import type * as Dom from './types.dom.js'
 import type { Variables } from './types.js'
-import type { ExtractableFile } from 'extract-files'
-import { extractFiles, isExtractableFile } from 'extract-files'
-import FormDataNode from 'form-data'
+// import type { ExtractableFile } from 'extract-files'
+// import { extractFiles, isExtractableFile } from 'extract-files'
+// import FormDataNode from 'form-data'
 
 /**
  * Duck type if NodeJS stream
  * https://github.com/sindresorhus/is-stream/blob/3750505b0727f6df54324784fe369365ef78841e/index.js#L3
  */
-const isExtractableFileEnhanced = (value: any): value is ExtractableFile | { pipe: Function } =>
-  isExtractableFile(value) ||
-  (value !== null && typeof value === `object` && typeof value.pipe === `function`)
+// const isExtractableFileEnhanced = (value: any): value is ExtractableFile | { pipe: Function } =>
+//   isExtractableFile(value) ||
+//   (value !== null && typeof value === `object` && typeof value.pipe === `function`)
 
 /**
  * Returns Multipart Form if body contains files
@@ -23,49 +23,49 @@ const createRequestBody = (
   variables?: Variables | Variables[],
   operationName?: string,
   jsonSerializer = defaultJsonSerializer
-): string | Dom.FormData => {
-  const { clone, files } = extractFiles({ query, variables, operationName }, ``, isExtractableFileEnhanced)
-
-  if (files.size === 0) {
-    if (!Array.isArray(query)) {
-      return jsonSerializer.stringify(clone)
-    }
-
-    if (typeof variables !== `undefined` && !Array.isArray(variables)) {
-      throw new Error(`Cannot create request body with given variable type, array expected`)
-    }
-
-    // Batch support
-    const payload = query.reduce<{ query: string; variables: Variables | undefined }[]>(
-      (accu, currentQuery, index) => {
-        accu.push({ query: currentQuery, variables: variables ? variables[index] : undefined })
-        return accu
-      },
-      []
-    )
-
-    return jsonSerializer.stringify(payload)
+): string => {
+  // const { clone, files } = extractFiles({ query, variables, operationName }, ``, isExtractableFileEnhanced)
+  const clone = { query, variables, operationName }
+  //if (files.size === 0) {
+  if (!Array.isArray(query)) {
+    return jsonSerializer.stringify(clone)
   }
 
-  const Form = typeof FormData === `undefined` ? FormDataNode : FormData
+  if (typeof variables !== `undefined` && !Array.isArray(variables)) {
+    throw new Error(`Cannot create request body with given variable type, array expected`)
+  }
 
-  const form = new Form()
+  // Batch support
+  const payload = query.reduce<{ query: string; variables: Variables | undefined }[]>(
+    (accu, currentQuery, index) => {
+      accu.push({ query: currentQuery, variables: variables ? variables[index] : undefined })
+      return accu
+    },
+    []
+  )
 
-  form.append(`operations`, jsonSerializer.stringify(clone))
+  return jsonSerializer.stringify(payload)
+  //}
 
-  const map: { [key: number]: string[] } = {}
-  let i = 0
-  files.forEach((paths) => {
-    map[++i] = paths
-  })
-  form.append(`map`, jsonSerializer.stringify(map))
+  // const Form = typeof FormData === `undefined` ? FormDataNode : FormData
 
-  i = 0
-  files.forEach((paths, file) => {
-    form.append(`${++i}`, file as any)
-  })
+  // const form = new Form()
 
-  return form as Dom.FormData
+  // form.append(`operations`, jsonSerializer.stringify(clone))
+
+  // const map: { [key: number]: string[] } = {}
+  // let i = 0
+  // files.forEach((paths) => {
+  //   map[++i] = paths
+  // })
+  // form.append(`map`, jsonSerializer.stringify(map))
+
+  // i = 0
+  // files.forEach((paths, file) => {
+  //   form.append(`${++i}`, file as any)
+  // })
+
+  // return form as Dom.FormData
 }
 
 export default createRequestBody
