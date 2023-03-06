26d3b7bfa6e03e7069a975574d91ba8130d7348e
Lyubomir Ivanov <livanov@openlinksw.bg>
2023-03-06 13:17:08 +0200

    fix: code formatting and type checks

--- tests/graphql-ws.test.ts
+++ tests/graphql-ws.test.ts
@@ -1,17 +1,19 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
-import gql from 'graphql-tag'
-import { useServer } from 'graphql-ws/lib/use/ws';
-import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws';
-import { GraphQLWebSocketClient } from '../src/graphql-ws.js';
-import getPort from 'get-port';
-import WebSocketImpl, { WebSocketServer } from 'ws';
-import { beforeAll, afterAll, expect, test } from 'vitest';
+import { gql } from 'graphql-tag'
+import { useServer } from 'graphql-ws/lib/use/ws'
+import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws'
+import { GraphQLWebSocketClient } from '../src/graphql-ws.js'
+import getPort from 'get-port'
+import WebSocketImpl, { WebSocketServer } from 'ws'
+import { beforeAll, afterAll, expect, test } from 'vitest'
 
 async function createClient(url: string) {
-    return new Promise<GraphQLWebSocketClient>((resolve) => {
-        const socket = new WebSocketImpl(url, GRAPHQL_TRANSPORT_WS_PROTOCOL);
-        const client: GraphQLWebSocketClient = new GraphQLWebSocketClient((socket as unknown) as WebSocket, { onAcknowledged: async (_p) => resolve(client) })
+  return new Promise<GraphQLWebSocketClient>((resolve) => {
+    const socket = new WebSocketImpl(url, GRAPHQL_TRANSPORT_WS_PROTOCOL)
+    const client: GraphQLWebSocketClient = new GraphQLWebSocketClient(socket as unknown as WebSocket, {
+      onAcknowledged: async (_p) => resolve(client),
     })
+  })
 }
 
 const typeDefs = gql`
@@ -21,58 +23,70 @@ const typeDefs = gql`
   type Subscription {
     greetings: String
   }
-`;
+`
 
 const resolvers = {
-    Query: {
-        hello: () => 'world',
-    },
-    Subscription: {
-        greetings: {
-            subscribe: async function* () {
-                for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
-                    yield { greetings: hi };
-                }
-            }
+  Query: {
+    hello: () => 'world',
+  },
+  Subscription: {
+    greetings: {
+      subscribe: async function* () {
+        for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+          yield { greetings: hi }
         }
-    }
-};
+      },
+    },
+  },
+}
 
-const schema = makeExecutableSchema({ typeDefs, resolvers }) 
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-var ctx: { server: WebSocketServer, url: string }
+var ctx: { server: WebSocketServer; url: string }
 
 beforeAll(async () => {
-    const port = await getPort()
-    const server = new WebSocketServer({ path: '/graphql', host: '127.0.0.1', port });
-    useServer({ schema }, server);
-    ctx = { server, url: `ws://localhost:${port}/graphql` }
+  const port = await getPort()
+  const server = new WebSocketServer({ path: '/graphql', host: '127.0.0.1', port })
+  useServer({ schema }, server)
+  ctx = { server, url: `ws://localhost:${port}/graphql` }
 })
 
 afterAll(() => {
-    ctx.server.close()
+  ctx.server.close()
 })
 
 test('graphql-ws request', async () => {
-    const client = await createClient(ctx.url)
-    const data = client.request(
-        gql`query hello { hello }`
-    )
-    expect(await data).toEqual({ hello: "world" })
-    client.close();
+  const client = await createClient(ctx.url)
+  const data = client.request(
+    gql`
+      query hello {
+        hello
+      }
+    `
+  )
+  expect(await data).toEqual({ hello: 'world' })
+  client.close()
 })
 
 test('graphql-ws subscription', async () => {
-    const client = await createClient(ctx.url)
-    const result = new Promise<string>((resolve) => {
-        var allGreatings = '';
-        client.subscribe<{ greetings: string }>(
-            gql`subscription greetings { greetings }`,
-            {
-                next: ({ greetings }) => allGreatings = allGreatings != '' ? `${allGreatings},${greetings}` : greetings,
-                complete: () => { resolve(allGreatings) }
-            })
-    })
-    expect(await result).toEqual("Hi,Bonjour,Hola,Ciao,Zdravo")
-    client.close();
+  const client = await createClient(ctx.url)
+  const result = new Promise<string>((resolve) => {
+    var allGreatings = ''
+    client.subscribe<{ greetings: string }>(
+      gql`
+        subscription greetings {
+          greetings
+        }
+      `,
+      {
+        next: ({ greetings }) =>
+          (allGreatings = allGreatings != '' ? `${allGreatings},${greetings}` : greetings),
+        complete: () => {
+          resolve(allGreatings)
+        },
+      }
+    )
+  })
+  expect(await result).toEqual('Hi,Bonjour,Hola,Ciao,Zdravo')
+  client.close()
 })
