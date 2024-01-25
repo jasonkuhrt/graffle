5379cd7bda345b6d02531b1cfe011b4253a9ad6e
Jason Kuhrt <jasonkuhrt@me.com>
2024-01-24 22:05:16 -0500

    fix

--- tests/$/testers.ts
+++ tests/$/testers.ts
@@ -1,22 +1,22 @@
-// import { test as testBase } from 'vitest'
+import { test as testBase } from 'vitest'
 
-// export const test = testBase.extend<Fixtures>({
-//   // // eslint-disable-next-line
-//   // c: async ({}, use) => {
-//   //   await use(context)
-//   //   await context.prisma.$disconnect()
-//   // },
-//   // userRecord: async ({ c }, use) => {
-//   //   const record = await setupUserRecord(c, { email: botEmail() })
-//   //   await use(record)
-//   // },
-//   // user: async ({ userRecord, c }, use) => {
-//   //   const token = await Janitor.Auth.forgeAccessTokenOrThrow(c, { userId: userRecord.id })
-//   //   await use({
-//   //     record: userRecord,
-//   //     token,
-//   //   })
-//   // },
-// })
+export const test = testBase.extend<Fixtures>({
+  // // eslint-disable-next-line
+  // c: async ({}, use) => {
+  //   await use(context)
+  //   await context.prisma.$disconnect()
+  // },
+  // userRecord: async ({ c }, use) => {
+  //   const record = await setupUserRecord(c, { email: botEmail() })
+  //   await use(record)
+  // },
+  // user: async ({ userRecord, c }, use) => {
+  //   const token = await Janitor.Auth.forgeAccessTokenOrThrow(c, { userId: userRecord.id })
+  //   await use({
+  //     record: userRecord,
+  //     token,
+  //   })
+  // },
+})
 
-// interface Fixtures {}
+interface Fixtures {}
