cccd996f3d96d4b7b1804ed4f83bc269d218fd1b
Jason Kuhrt <jasonkuhrt@me.com>
2024-02-28 00:12:15 -0500

    format

--- tests/builder/_/schema.ts
+++ tests/builder/_/schema.ts
@@ -1,34 +1,35 @@
 export namespace $ {
-export interface Index {
-Root: {
-Query: Root.Query
-Mutation: null
-Subscription: null
-}
-objects: {
-Foo: Object.Foo
-Bar: Object.Bar
-ObjectNested: Object.ObjectNested
-Object: Object.Object
-Object1ImplementingInterface: Object.Object1ImplementingInterface
-Object2ImplementingInterface: Object.Object2ImplementingInterface
-}
-unionMemberNames: {
-FooBarUnion: "Foo"
-| "Bar"
-}
-unions: {
-Union: Union.FooBarUnion
-}
-scalars: Scalars
-}
-export interface Scalars {
-ID: string
-String: string
-Int: number
-Float: number
-Boolean: boolean
-}
+  export interface Index {
+    Root: {
+      Query: Root.Query
+      Mutation: null
+      Subscription: null
+    }
+    objects: {
+      Foo: Object.Foo
+      Bar: Object.Bar
+      ObjectNested: Object.ObjectNested
+      Object: Object.Object
+      Object1ImplementingInterface: Object.Object1ImplementingInterface
+      Object2ImplementingInterface: Object.Object2ImplementingInterface
+    }
+    unionMemberNames: {
+      FooBarUnion:
+        | 'Foo'
+        | 'Bar'
+    }
+    unions: {
+      Union: Union.FooBarUnion
+    }
+    scalars: Scalars
+  }
+  export interface Scalars {
+    ID: string
+    String: string
+    Int: number
+    Float: number
+    Boolean: boolean
+  }
 }
 
 // ------------------------------------------------------------ //
@@ -36,233 +37,233 @@ Boolean: boolean
 // ------------------------------------------------------------ //
 
 export namespace Root {
-export interface Query {
-__typename: {
-type: {
-kind: "literal"
-value: "Query"
-}
-args: null
-namedType: "Query"
-}
-interface: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Interface.Interface
-}
-}
-namedType: Interface.Interface
-args: null
-}
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-idNonNull: {
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-string: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["String"]
-}
-}
-namedType: $.Scalars["String"]
-args: null
-}
-stringWithRequiredArg: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["String"]
-}
-}
-namedType: $.Scalars["String"]
-args: {
-type: {
-string: $.Scalars["String"]
-}
-allOptional: false
-}
-}
-stringWithArgs: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["String"]
-}
-}
-namedType: $.Scalars["String"]
-args: {
-type: {
-string?: $.Scalars["String"] | null
-int?: $.Scalars["Int"] | null
-float?: $.Scalars["Float"] | null
-boolean?: $.Scalars["Boolean"] | null
-id?: $.Scalars["ID"] | null
-}
-allOptional: true
-}
-}
-object: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Object.Object
-}
-}
-namedType: Object.Object
-args: null
-}
-listListIntNonNull: {
-type: {
-kind: "list"
-type: {
-kind: "list"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-listListInt: {
-type: {
-kind: "nullable"
-type: {
-kind: "list"
-type: {
-kind: "nullable"
-type: {
-kind: "list"
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-}
-}
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-listInt: {
-type: {
-kind: "nullable"
-type: {
-kind: "list"
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-listIntNonNull: {
-type: {
-kind: "list"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-objectNested: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Object.ObjectNested
-}
-}
-namedType: Object.ObjectNested
-args: null
-}
-objectNonNull: {
-type: {
-kind: "named"
-named: Object.Object
-}
-namedType: Object.Object
-args: null
-}
-objectWithArgs: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Object.Object
-}
-}
-namedType: Object.Object
-args: {
-type: {
-string?: $.Scalars["String"] | null
-int?: $.Scalars["Int"] | null
-float?: $.Scalars["Float"] | null
-boolean?: $.Scalars["Boolean"] | null
-id?: $.Scalars["ID"] | null
-}
-allOptional: true
-}
-}
-fooBarUnion: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Union.FooBarUnion
-}
-}
-namedType: Union.FooBarUnion
-args: null
-}
-/**
-* Query enum field documentation.
-*/
-abcEnum: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Enum.ABCEnum
-}
-}
-namedType: Enum.ABCEnum
-args: null
-}
-}
+  export interface Query {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Query'
+      }
+      args: null
+      namedType: 'Query'
+    }
+    interface: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Interface.Interface
+        }
+      }
+      namedType: Interface.Interface
+      args: null
+    }
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+    idNonNull: {
+      type: {
+        kind: 'named'
+        named: $.Scalars['ID']
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+    string: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['String']
+        }
+      }
+      namedType: $.Scalars['String']
+      args: null
+    }
+    stringWithRequiredArg: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['String']
+        }
+      }
+      namedType: $.Scalars['String']
+      args: {
+        type: {
+          string: $.Scalars['String']
+        }
+        allOptional: false
+      }
+    }
+    stringWithArgs: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['String']
+        }
+      }
+      namedType: $.Scalars['String']
+      args: {
+        type: {
+          string?: $.Scalars['String'] | null
+          int?: $.Scalars['Int'] | null
+          float?: $.Scalars['Float'] | null
+          boolean?: $.Scalars['Boolean'] | null
+          id?: $.Scalars['ID'] | null
+        }
+        allOptional: true
+      }
+    }
+    object: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Object.Object
+        }
+      }
+      namedType: Object.Object
+      args: null
+    }
+    listListIntNonNull: {
+      type: {
+        kind: 'list'
+        type: {
+          kind: 'list'
+          type: {
+            kind: 'named'
+            named: $.Scalars['Int']
+          }
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+    listListInt: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'list'
+          type: {
+            kind: 'nullable'
+            type: {
+              kind: 'list'
+              type: {
+                kind: 'nullable'
+                type: {
+                  kind: 'named'
+                  named: $.Scalars['Int']
+                }
+              }
+            }
+          }
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+    listInt: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'list'
+          type: {
+            kind: 'nullable'
+            type: {
+              kind: 'named'
+              named: $.Scalars['Int']
+            }
+          }
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+    listIntNonNull: {
+      type: {
+        kind: 'list'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Int']
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+    objectNested: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Object.ObjectNested
+        }
+      }
+      namedType: Object.ObjectNested
+      args: null
+    }
+    objectNonNull: {
+      type: {
+        kind: 'named'
+        named: Object.Object
+      }
+      namedType: Object.Object
+      args: null
+    }
+    objectWithArgs: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Object.Object
+        }
+      }
+      namedType: Object.Object
+      args: {
+        type: {
+          string?: $.Scalars['String'] | null
+          int?: $.Scalars['Int'] | null
+          float?: $.Scalars['Float'] | null
+          boolean?: $.Scalars['Boolean'] | null
+          id?: $.Scalars['ID'] | null
+        }
+        allOptional: true
+      }
+    }
+    fooBarUnion: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Union.FooBarUnion
+        }
+      }
+      namedType: Union.FooBarUnion
+      args: null
+    }
+    /**
+     * Query enum field documentation.
+     */
+    abcEnum: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Enum.ABCEnum
+        }
+      }
+      namedType: Enum.ABCEnum
+      args: null
+    }
+  }
 }
 
 // ------------------------------------------------------------ //
@@ -270,18 +271,18 @@ args: null
 // ------------------------------------------------------------ //
 
 export namespace Enum {
-/**
-* Enum documentation.
-* 
-* Members
-* "A" - (DEPRECATED: Enum value A is deprecated.)
-* "B" - Enum B member documentation.
-* "C" - Enum C member documentation. (DEPRECATED: Enum value C is deprecated.)
-*/
-export type ABCEnum =
-| "A"
-| "B"
-| "C"
+  /**
+   * Enum documentation.
+   *
+   * Members
+   * "A" - (DEPRECATED: Enum value A is deprecated.)
+   * "B" - Enum B member documentation.
+   * "C" - Enum C member documentation. (DEPRECATED: Enum value C is deprecated.)
+   */
+  export type ABCEnum =
+    | 'A'
+    | 'B'
+    | 'C'
 }
 
 // ------------------------------------------------------------ //
@@ -289,8 +290,7 @@ export type ABCEnum =
 // ------------------------------------------------------------ //
 
 export namespace InputObject {
-// -- no types --
-
+  // -- no types --
 }
 
 // ------------------------------------------------------------ //
@@ -298,24 +298,25 @@ export namespace InputObject {
 // ------------------------------------------------------------ //
 
 export namespace Interface {
-export interface Interface {
-__interfacename: "Interface"
-type: {
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-}
-implementors: Object.Object1ImplementingInterface
-| Object.Object2ImplementingInterface
-}
+  export interface Interface {
+    __interfacename: 'Interface'
+    type: {
+      id: {
+        type: {
+          kind: 'nullable'
+          type: {
+            kind: 'named'
+            named: $.Scalars['ID']
+          }
+        }
+        namedType: $.Scalars['ID']
+        args: null
+      }
+    }
+    implementors:
+      | Object.Object1ImplementingInterface
+      | Object.Object2ImplementingInterface
+  }
 }
 
 // ------------------------------------------------------------ //
@@ -323,222 +324,222 @@ implementors: Object.Object1ImplementingInterface
 // ------------------------------------------------------------ //
 
 export namespace Object {
-/**
-* Object documentation.
-*/
-export interface Foo {
-__typename: {
-type: {
-kind: "literal"
-value: "Foo"
-}
-args: null
-namedType: "Foo"
-}
-/**
-* Field documentation.
-* 
-* @deprecated Field a is deprecated.
-*/
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-}
+  /**
+   * Object documentation.
+   */
+  export interface Foo {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Foo'
+      }
+      args: null
+      namedType: 'Foo'
+    }
+    /**
+     * Field documentation.
+     *
+     * @deprecated Field a is deprecated.
+     */
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+  }
 
-export interface Bar {
-__typename: {
-type: {
-kind: "literal"
-value: "Bar"
-}
-args: null
-namedType: "Bar"
-}
-int: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-}
+  export interface Bar {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Bar'
+      }
+      args: null
+      namedType: 'Bar'
+    }
+    int: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Int']
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+  }
 
-export interface ObjectNested {
-__typename: {
-type: {
-kind: "literal"
-value: "ObjectNested"
-}
-args: null
-namedType: "ObjectNested"
-}
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-object: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: Object.Object
-}
-}
-namedType: Object.Object
-args: null
-}
-}
+  export interface ObjectNested {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'ObjectNested'
+      }
+      args: null
+      namedType: 'ObjectNested'
+    }
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+    object: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: Object.Object
+        }
+      }
+      namedType: Object.Object
+      args: null
+    }
+  }
 
-export interface Object {
-__typename: {
-type: {
-kind: "literal"
-value: "Object"
-}
-args: null
-namedType: "Object"
-}
-string: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["String"]
-}
-}
-namedType: $.Scalars["String"]
-args: null
-}
-int: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-float: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Float"]
-}
-}
-namedType: $.Scalars["Float"]
-args: null
-}
-boolean: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Boolean"]
-}
-}
-namedType: $.Scalars["Boolean"]
-args: null
-}
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-}
+  export interface Object {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Object'
+      }
+      args: null
+      namedType: 'Object'
+    }
+    string: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['String']
+        }
+      }
+      namedType: $.Scalars['String']
+      args: null
+    }
+    int: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Int']
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+    float: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Float']
+        }
+      }
+      namedType: $.Scalars['Float']
+      args: null
+    }
+    boolean: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Boolean']
+        }
+      }
+      namedType: $.Scalars['Boolean']
+      args: null
+    }
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+  }
 
-export interface Object1ImplementingInterface {
-__typename: {
-type: {
-kind: "literal"
-value: "Object1ImplementingInterface"
-}
-args: null
-namedType: "Object1ImplementingInterface"
-}
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-int: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Int"]
-}
-}
-namedType: $.Scalars["Int"]
-args: null
-}
-}
+  export interface Object1ImplementingInterface {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Object1ImplementingInterface'
+      }
+      args: null
+      namedType: 'Object1ImplementingInterface'
+    }
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+    int: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Int']
+        }
+      }
+      namedType: $.Scalars['Int']
+      args: null
+    }
+  }
 
-export interface Object2ImplementingInterface {
-__typename: {
-type: {
-kind: "literal"
-value: "Object2ImplementingInterface"
-}
-args: null
-namedType: "Object2ImplementingInterface"
-}
-id: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["ID"]
-}
-}
-namedType: $.Scalars["ID"]
-args: null
-}
-boolean: {
-type: {
-kind: "nullable"
-type: {
-kind: "named"
-named: $.Scalars["Boolean"]
-}
-}
-namedType: $.Scalars["Boolean"]
-args: null
-}
-}
+  export interface Object2ImplementingInterface {
+    __typename: {
+      type: {
+        kind: 'literal'
+        value: 'Object2ImplementingInterface'
+      }
+      args: null
+      namedType: 'Object2ImplementingInterface'
+    }
+    id: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['ID']
+        }
+      }
+      namedType: $.Scalars['ID']
+      args: null
+    }
+    boolean: {
+      type: {
+        kind: 'nullable'
+        type: {
+          kind: 'named'
+          named: $.Scalars['Boolean']
+        }
+      }
+      namedType: $.Scalars['Boolean']
+      args: null
+    }
+  }
 }
 
 // ------------------------------------------------------------ //
@@ -546,12 +547,13 @@ args: null
 // ------------------------------------------------------------ //
 
 export namespace Union {
-/**
-* Union documentation.
-*/
-export interface FooBarUnion {
-__unionname: "FooBarUnion"
-type: Object.Foo
-| Object.Bar
-}
+  /**
+   * Union documentation.
+   */
+  export interface FooBarUnion {
+    __unionname: 'FooBarUnion'
+    type:
+      | Object.Foo
+      | Object.Bar
+  }
 }
