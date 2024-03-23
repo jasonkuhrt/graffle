b3f579cfb5349a514241ee3a76b4c0441993a4e9
Jason Kuhrt <jasonkuhrt@me.com>
2024-03-22 20:34:03 -0400

    feat: introduce a CLI for generating the client

--- src/cli/generate.ts
+++ src/cli/generate.ts
@@ -1,6 +1,21 @@
-import { generateFile } from '../generator/generator.js'
+#!/usr/bin/env node
 
-await generateFile({
-  schemaPath: `./examples/schema.graphql`,
-  typeScriptPath: `./src/demo.ts`,
-})
+import { Command } from '@molt/command'
+import * as fs from 'node:fs/promises'
+import { z } from 'zod'
+import { generateCode } from '../generator/generator.js'
+
+const args = Command.create().description(`Generate a type safe GraphQL client.`)
+  .parameter(`schemaPath`, z.string().min(1).describe(`File path to where your GraphQL schema is.`))
+  .parameter(
+    `output`,
+    z.string().min(1).optional().describe(`File path for where to output the generated TypeScript types.`),
+  )
+  .parse()
+
+const schemaSource = await fs.readFile(args.schemaPath, `utf8`)
+const code = generateCode({ schemaSource })
+
+if (args.output) {
+  await fs.writeFile(args.output, code, { encoding: `utf8` })
+}
