5bf20c30e92dba230f0c0f59d43106f3e3fc596b
Jason Kuhrt <jasonkuhrt@me.com>
2024-03-31 11:15:50 -0400

    test cases

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { setupMockServer } from '../tests/raw/__helpers.js'
 import type { $ } from '../tests/ts/_/schema/SchemaBuildtime.js'
 import { $Index as schemaIndex } from '../tests/ts/_/schema/SchemaRuntime.js'
@@ -16,4 +16,20 @@ test(`query`, async () => {
     schemaIndex,
   })
   expect(await client.query({ fooBarUnion: { onBar: { int: true } } })).toEqual(mockRes.data)
+})
+
+describe(`custom scalar`, () => {
+  describe(`output`, () => {
+    test.todo(`query field`)
+    test.todo(`query field in non-null`)
+    test.todo(`query field in list`)
+    test.todo(`query field in list non-null`)
+    test.todo(`object field`)
+    test.todo(`object field in union`)
+    test.todo(`object field in interface`)
+  })
+  describe(`input`, () => {
+    test.todo(`arg field`)
+    test.todo(`input object field`)
+  })
 })
