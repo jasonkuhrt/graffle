9e0c896b895664e63cfb0396b2302ca58e2bb64c
Jason Kuhrt <jasonkuhrt@me.com>
2024-04-10 23:47:39 -0400

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,12 @@ import configPrisma from 'eslint-config-prisma'
 import tsEslint from 'typescript-eslint'
 
 export default tsEslint.config({
-  ignores: [`./build/`, 'eslint.config.js'],
-  extends: [...configPrisma],
+  ignores: ['**/build/**/*', 'eslint.config.js'],
+  extends: configPrisma,
   languageOptions: {
     parserOptions: {
       project: true,
       tsconfigRootDir: import.meta.dirname,
     },
-  },
-  rules: {
-    '@typescript-eslint/ban-ts-comment': ['warn', { 'ts-expect-error': false }],
   },
 })
