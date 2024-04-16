a418ae8cc99ce03a5fc45f9d3e8d2739c32e96ff
Jason Kuhrt <jasonkuhrt@me.com>
2024-04-16 16:30:15 +0200

    more cases

--- src/client/client.document.test.ts
+++ src/client/client.document.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import type { Index } from '../../tests/_/schema/generated/Index.js'
 import { $Index } from '../../tests/_/schema/generated/SchemaRuntime.js'
 import { db, schema } from '../../tests/_/schema/schema.js'
@@ -6,23 +6,39 @@ import { create } from './client.js'
 
 const client = create<Index>({ schema, schemaIndex: $Index })
 
-test(`document with two queries`, async () => {
-  const { run } = client.document({
-    foo: {
-      query: { id: true },
-    },
-    bar: {
-      query: { idNonNull: true },
-    },
+const withTwo = client.document({
+  foo: {
+    query: { id: true },
+  },
+  bar: {
+    query: { idNonNull: true },
+  },
+})
+
+describe(`document with two queries`, () => {
+  test(`works`, async () => {
+    const { run } = withTwo
+    expect(await run(`foo`)).toEqual({ data: { id: db.id1 } })
+    expect(await run(`bar`)).toEqual({ data: { idNonNull: db.id1 } })
   })
-  expect(await run(`foo`)).toEqual({ data: { id: db.id1 } })
-  expect(await run(`bar`)).toEqual({ data: { idNonNull: db.id1 } })
-  // @ts-expect-error
-  const result = await run().catch((e: unknown) => e) as { errors: Error[] }
-  expect(result.errors.length).toBe(1)
-  const error = result.errors[0]!
-  expect(error instanceof Error).toBe(true)
-  expect(error.message).toMatch(`Must provide operation name if query contains multiple operations`)
+  test(`error if no operation name is provided`, async () => {
+    const { run } = withTwo
+    // @ts-expect-error
+    const result = await run().catch((e: unknown) => e) as { errors: Error[] }
+    expect(result.errors.length).toBe(1)
+    const error = result.errors[0]!
+    expect(error instanceof Error).toBe(true)
+    expect(error.message).toMatch(`Must provide operation name if query contains multiple operations`)
+  })
+  test(`error if wrong operation name is provided`, async () => {
+    const { run } = withTwo
+    // @ts-expect-error
+    const result = await run(`boo`).catch((e: unknown) => e) as { errors: Error[] }
+    expect(result.errors.length).toBe(1)
+    const error = result.errors[0]!
+    expect(error instanceof Error).toBe(true)
+    expect(error.message).toMatch(`Unknown operation named "boo"`)
+  })
 })
 
 test(`document with one query`, async () => {
@@ -36,6 +52,5 @@ test(`document with one query`, async () => {
   expect(await run(undefined)).toEqual({ data: { id: db.id1 } })
 })
 
-// todo passing wrong operation name should throw
 // todo mutation
 // todo mutation and query mixed
