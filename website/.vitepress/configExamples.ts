e44d32bc8d2cf0fb145b892885efb082251d23a4
Jason Kuhrt <jasonkuhrt@me.com>
2024-08-30 21:10:25 -0400

    format

--- website/.vitepress/configExamples.ts
+++ website/.vitepress/configExamples.ts
@@ -1,42 +1,41 @@
+import { DefaultTheme } from 'vitepress'
 
-	import { DefaultTheme } from 'vitepress'
-
-	export const sidebarExamples:DefaultTheme.SidebarItem[] = [
+export const sidebarExamples: DefaultTheme.SidebarItem[] = [
   {
-    "text": "Transport Memory",
-    "link": "/examples/transport-memory"
+    'text': 'Transport Memory',
+    'link': '/examples/transport-memory',
   },
   {
-    "text": "Transport Http Headers",
-    "link": "/examples/transport-http_headers"
+    'text': 'Transport Http Headers',
+    'link': '/examples/transport-http_headers',
   },
   {
-    "text": "Transport Http Fetch",
-    "link": "/examples/transport-http_fetch"
+    'text': 'Transport Http Fetch',
+    'link': '/examples/transport-http_fetch',
   },
   {
-    "text": "Raw Typed",
-    "link": "/examples/raw-typed"
+    'text': 'Raw Typed',
+    'link': '/examples/raw-typed',
   },
   {
-    "text": "RawString Typed",
-    "link": "/examples/rawString-typed"
+    'text': 'RawString Typed',
+    'link': '/examples/rawString-typed',
   },
   {
-    "text": "RawString",
-    "link": "/examples/rawString"
+    'text': 'RawString',
+    'link': '/examples/rawString',
   },
   {
-    "text": "Raw",
-    "link": "/examples/raw"
+    'text': 'Raw',
+    'link': '/examples/raw',
   },
   {
-    "text": "Generated",
-    "items": [
+    'text': 'Generated',
+    'items': [
       {
-        "text": "Arguments",
-        "link": "/examples/generated-arguments"
-      }
-    ]
-  }
+        'text': 'Arguments',
+        'link': '/examples/generated-arguments',
+      },
+    ],
+  },
 ]
--- website/content/examples/transport-http_fetch.md
+++ website/content/examples/transport-http_fetch.md
@@ -15,14 +15,20 @@ const graffle = Graffle
       return await exchange({
         using: {
           fetch: async () => {
-            return new Response(JSON.stringify({ data: { countries: [{ name: `Canada Mocked!` }] } }))
+            return new Response(
+              JSON.stringify({
+                data: { countries: [{ name: `Canada Mocked!` }] },
+              }),
+            )
           },
         },
       })
     },
   })
 
-const countries = await graffle.rawString({ document: `{ countries { name } }` })
+const countries = await graffle.rawString({
+  document: `{ countries { name } }`,
+})
 
 console.log(countries.data)
 //          ^?
