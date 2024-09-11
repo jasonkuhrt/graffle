d74e2452a9f00c98e6d0b04b201a5497f5da7bf2
Jason Kuhrt <jasonkuhrt@me.com>
2024-09-11 11:33:05 -0400

    update

--- examples/__outputs__/extension|extension_opentelemetry.output.encoder.ts
+++ examples/__outputs__/extension|extension_opentelemetry.output.encoder.ts
@@ -5,6 +5,6 @@ export const encode = (value: string) => {
     .replaceAll(/(parentId: )'.+'/g, `$1'...'`)
     .replaceAll(/(timestamp: ).+,/g, `$10,`)
     .replaceAll(/(duration: ).+,/g, `$10.0,`)
-    .replaceAll(/(service\.name: )'.+'/g, `$1'...'`)
+    .replaceAll(/(service\.name': )'.+'/g, `$1'...'`)
     .replaceAll(/('telemetry\.sdk\.version': )'.+'/g, `$1'...'`)
 }
