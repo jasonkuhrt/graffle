4944284db3bbbcc86b51960aac48370e42845311
Jason Kuhrt <jasonkuhrt@me.com>
2024-09-20 12:58:46 -0400

    fix

--- src/layers/3_SelectionSet/encode.ts
+++ src/layers/3_SelectionSet/encode.ts
@@ -88,7 +88,6 @@ const resolveArgValue = (
       return String(argValue)
     }
     case `Scalar`: {
-      // @ts-expect-error fixme
       return JSON.stringify(schemaArgType.codec.encode(argValue))
     }
     default:
