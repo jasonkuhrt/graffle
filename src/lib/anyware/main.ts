5e637ed8e4bd0e73e418eee25f2a9cb383c75af6
Jason Kuhrt <jasonkuhrt@me.com>
2024-09-25 15:15:00 -0400

    doc

--- src/lib/anyware/main.ts
+++ src/lib/anyware/main.ts
@@ -164,7 +164,21 @@ export type CoreInput<
         >
       }
   }
+  /**
+   * If a hook results in a thrown error but is an instance of one of these classes then return it as-is
+   * rather than wrapping it in a ContextualError.
+   *
+   * This can be useful when there are known kinds of errors such as Abort Errors from AbortController
+   * which are actually a signaling mechanism.
+   */
   passthroughErrorInstanceOf?: Function[]
+  /**
+   * If a hook results in a thrown error but returns true from this function then return the error as-is
+   * rather than wrapping it in a ContextualError.
+   *
+   * This can be useful when there are known kinds of errors such as Abort Errors from AbortController
+   * which are actually a signaling mechanism.
+   */
   passthroughErrorWith?: (signal: HookResultError) => boolean
 }
 
