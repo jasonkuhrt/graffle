bd10b762714fbd3e6780ffdae820e25ec0a08139
Jason Kuhrt <jasonkuhrt@me.com>
2024-10-18 10:45:56 -0400

    unit tests

--- src/layers/4_generator/config/config.test.ts
+++ src/layers/4_generator/config/config.test.ts
@@ -6,7 +6,7 @@ test(`can load schema from custom path`, async () => {
   const customPathFile = `./tests/_/fixtures/custom.graphql`
   const config = await createConfig({ schema: { type: `sdl`, dirOrFilePath: customPathFile } })
   const field = config.schema.instance.getQueryType()?.getFields()[`customNamedSchemaFile`]
-  expect(config.paths.project.inputs.schema).toEqual(customPathFile)
+  expect(config.paths.project.inputs.schema).match(new RegExp(customPathFile + `$`))
   expect(config.schema.sdl).toMatchSnapshot()
   expect(field).toBeDefined()
 })
@@ -15,7 +15,7 @@ test(`can load schema from custom dir using default file name`, async () => {
   const customPathDir = `tests/_/fixtures`
   const config = await createConfig({ schema: { type: `sdl`, dirOrFilePath: customPathDir } })
   const field = config.schema.instance.getQueryType()?.getFields()[`defaultNamedSchemaFile`]
-  expect(config.paths.project.inputs.schema).toEqual(customPathDir + `/schema.graphql`)
+  expect(config.paths.project.inputs.schema).match(new RegExp(customPathDir + `/schema.graphql$`))
   expect(config.schema.sdl).toMatchSnapshot()
   expect(field).toBeDefined()
 })
