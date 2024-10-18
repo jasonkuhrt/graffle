220542697eab6884ac761925fef32f87ef389658
Jason Kuhrt <jasonkuhrt@me.com>
2024-10-18 12:07:45 -0400

    update snap

--- src/layers/4_generator/generators/global.ts
+++ src/layers/4_generator/generators/global.ts
@@ -1,4 +1,5 @@
 import { Code } from '../../../lib/Code.js'
+import { identifiers } from '../helpers/identifiers.js'
 import { createModuleGenerator } from '../helpers/moduleGenerator.js'
 import { ModuleGeneratorData } from './Data.js'
 import { ModuleGeneratorMethodsDocument } from './MethodsDocument.js'
@@ -10,16 +11,12 @@ import { ModuleGeneratorSchema } from './Schema.js'
 export const ModuleGeneratorGlobal = createModuleGenerator(
   `Global`,
   ({ config, code }) => {
-    const identifiers = {
-      Scalar: `Scalar`,
-    }
-
     code(
       `import type * as Data from './${ModuleGeneratorData.name}.js'`,
       `import type * as MethodsSelect from './${ModuleGeneratorMethodsSelect.name}.js'`,
       `import type * as MethodsDocument from './${ModuleGeneratorMethodsDocument.name}.js'`,
       `import type * as MethodsRoot from './${ModuleGeneratorMethodsRoot.name}.js'`,
-      `import type { Schema } from './${ModuleGeneratorSchema.name}.js'`,
+      `import type { ${identifiers.Schema} } from './${ModuleGeneratorSchema.name}.js'`,
     )
 
     if (config.schema.kindMap.GraphQLScalarTypeCustom.length > 0) {
@@ -37,7 +34,7 @@ export const ModuleGeneratorGlobal = createModuleGenerator(
     const ClientFields = Code.termObjectFields({
       [config.name]: {
         name: `Data.Name`,
-        schema: `Schema`,
+        schema: identifiers.Schema,
         interfaces: {
           MethodsSelect: `MethodsSelect.$MethodsSelect`,
           Document: `MethodsDocument.BuilderMethodsDocumentFn`,
@@ -51,6 +48,7 @@ export const ModuleGeneratorGlobal = createModuleGenerator(
       },
     })
 
+    // todo rename "Schemas" to "Clients"
     code(`
       declare global {
         export namespace GraffleGlobal {
--- src/layers/4_generator/helpers/identifiers.ts
+++ src/layers/4_generator/helpers/identifiers.ts
@@ -2,4 +2,5 @@ export const identifiers = {
   Schema: `Schema`,
   Utilities: `Utilities`,
   MethodsRoot: `MethodsRoot`,
+  Scalar: `Scalar`,
 }
